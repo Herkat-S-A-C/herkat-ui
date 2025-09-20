5ba4d710bed7ddcea651ff727f4869a2693db10f
Robert-Otiniano <robertalexis2016@gmail.com>
2025-09-19 21:50:24 -0500

    fixing bugs in ModalForm

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-=======
-import { Motion } from "framer-motion";
 import { formConfig } from "../constants/formConfig";
 import FormFields from "./FormFields";
 
